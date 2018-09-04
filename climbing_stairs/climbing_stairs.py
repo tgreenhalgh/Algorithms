aafb5fa4038590dfdb11243eb9df1ea5a5b17292
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-09-04 12:34:32 -0700

    stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,15 +2,25 @@
 
 import sys
 
+
 def climbing_stairs(n):
-  pass
+    ways = [0 for x in range(n + 2)]  # we use 2 in case n = 0
+    ways[0], ways[1] = 1, 1
+
+    for i in range(2, n + 1):
+        j = 1
+        while j <= 3 and j <= i:
+            ways[i] = ways[i] + ways[i-j]
+            j += 1
+    return ways[n]
 
 
 if __name__ == "__main__":
-  # Test out your implementation from the command line
-  # with `python climbing_stairs.py [n]` with different n values
-  if len(sys.argv) > 1:
-    num_stairs = int(sys.argv[1])
-    print("There are {ways} ways for a child to jump {n} stairs.".format(ways=climbing_stairs(num_stairs), n=num_stairs))
-  else:
-    print('Usage: climbing_stairs.py [num_stairs]')
+    # Test out your implementation from the command line
+    # with `python climbing_stairs.py [n]` with different n values
+    if len(sys.argv) > 1:
+        num_stairs = int(sys.argv[1])
+        print("There are {ways} ways for a child to jump {n} stairs.".format(
+            ways=climbing_stairs(num_stairs), n=num_stairs))
+    else:
+        print('Usage: climbing_stairs.py [num_stairs]')
--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,40 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass
+    count = 0
+    check = True
+
+    for key in recipe:
+        if key not in ingredients:
+            return 0
+
+    while check:
+        keys = 0
+        for key in recipe:
+            keys += 1
+            if (check and recipe[key] * (count + 1)) <= ingredients[key]:
+                check = True
+            else:
+                check = False
+                break
+            if check and keys == len(recipe):
+                count += 1
+
+    if count:
+        return count
+    else:
+        return 0
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+            # Change the entries of these dictionaries to test
+            # your implementation with different inputs
+    # recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    # ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    recipe = {'milk': 2, 'sugar': 40, 'butter': 20}
+    ingredients = {'milk': 5, 'sugar': 120, 'butter': 500}
+
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
