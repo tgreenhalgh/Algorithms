f63e6aa140e537e2e94861de25c498744b6496d8
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-09-04 13:27:04 -0700

    rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,14 +2,34 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    if n > 0:
+        list = [["rock"], ["paper"], ["scissors"]]
+        results = []
+
+        def recurse(plays):
+            if len(plays) == n:
+                results.append(plays)
+                return
+
+            for l in list:
+                recurse(plays + l)
+
+        recurse([])
+
+        return results
+
+    return [[]]
+
 
 if __name__ == "__main__":
-  # Test out your implementation from the command line
-  # with `python rps.py [n]` with different n values
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    # Test out your implementation from the command line
+    # with `python rps.py [n]` with different n values
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
+
+# print(rock_paper_scissors(2))
